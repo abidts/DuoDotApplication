7ba93785312cf8ed3a72112ae6338c3df36d87f0
ABC <aabidshora@gmail.com>
2026-05-15 16:35:46 +0530

    Update AuthenticationService.java

--- src/main/java/com/duodot/service/AuthenticationService.java
+++ src/main/java/com/duodot/service/AuthenticationService.java
@@ -75,6 +75,7 @@ public class AuthenticationService {
                     .userId(userId)
                     .createdDate(Calendar.getInstance())
                     .paired(Boolean.FALSE)
+                    .isDeleted(Boolean.FALSE)
                     .pairIds(new ArrayList<>())
                     .accountStatus(UserStatusEnum.VERIFICATION_PENDING.getValue())
                     .build();
