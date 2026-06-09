9ca6311d61a1fcb99899f706bffeef53f0bedacf
abidts <aabidshora@gmail.com>
2026-06-10 00:27:49 +0530

    Update MemoryController.java

--- src/main/java/com/duodot/controller/MemoryController.java
+++ src/main/java/com/duodot/controller/MemoryController.java
@@ -3,6 +3,7 @@ package com.duodot.controller;
 import com.duodot.requestBean.MemoryRequestBean;
 import com.duodot.responseBean.ServiceResponseBean;
 import com.duodot.service.MemoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -19,12 +20,14 @@ import java.util.List;
 public class MemoryController {
 
     private final MemoryService memoryService;
+    private final ObjectMapper objectMapper;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ServiceResponseBean createMemory(
-            @Valid @RequestPart("request") MemoryRequestBean request,
+            @RequestParam("request") String requestJson,
             @RequestPart(value = "files", required = false) List<MultipartFile> files
-    ) {
+    ) throws Exception {
+        MemoryRequestBean request = objectMapper.readValue(requestJson, MemoryRequestBean.class);
         ServiceResponseBean serviceResponseBean = new ServiceResponseBean();
         serviceResponseBean = memoryService.createMemory(request, files, serviceResponseBean);
         return serviceResponseBean;
